91738c795472d07d87f5a4b830817b1654e553bf
shelly13black <shellymashilo@gmail.com>
2022-03-02 14:52:06 +0200

    JWT token + RestTemplate example.

---
+++ src/main/java/com/example/jwtresttemplate/JwtRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtresttemplate/JwtRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
